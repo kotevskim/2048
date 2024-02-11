6f75be50822a2dcb0748ab292ce5846acd868517
martin.kotevski <martin.kotevski@sorsix.com>
2024-02-11 20:43:53 +0100

    init board

---
+++ components/board.tsx
@@ -0,0 +1,18 @@
+import styles from "@/styles/board.module.css";
+
+export default function Board() {
+  const renderGrid = () => {
+    const cells: JSX.Element[] = [];
+    const cellsCount = 16;
+    for (let index = 0; index < cellsCount; index += 1) {
+      cells.push(<div className={styles.cell} key={index}></div>);
+    }
+    return cells;
+  };
+
+  return (
+    <div className={styles.board}>
+      <div className={styles.grid}>{renderGrid()}</div>
+    </div>
+  );
+}
