a33794ad22b2e21a91044a4c48e7bef41b018fae
martin.kotevski <martin.kotevski@sorsix.com>
2024-02-12 01:27:10 +0100

    merge animation using custom usePreviousProps hook

--- components/tile.tsx
+++ components/tile.tsx
@@ -1,8 +1,21 @@
 import styles from "@/styles/tile.module.css";
 import { Tile as TileProps } from "@/models/tile";
-import { containerWidth, tileCountPerDimesion } from "@/constants";
+import { containerWidth, mergeAnimationDuration, tileCountPerDimesion } from "@/constants";
+import { useEffect, useState } from "react";
+import usePreviousProps from "@/hooks/use-previous-props";
 
 export default function Tile({ position, value }: TileProps) {
+  const [scale, setScale] = useState(1);
+  const previousValue = usePreviousProps(value);
+  const hasChanged = previousValue != value;
+
+  useEffect(() => {
+    if (hasChanged) {
+      setScale(1.1);
+      setTimeout(() => setScale(1), mergeAnimationDuration);
+    }
+  }, [hasChanged]); // trigger useEffect only when the the value is changing
+
   const positionToPixels = (position: number) => {
     // 0: (0 / 4) * 288 = 0px
     // 1: (1 / 4) * 288 = 72px
@@ -11,6 +24,7 @@ export default function Tile({ position, value }: TileProps) {
   const style = {
     left: positionToPixels(position[0]),
     top: positionToPixels(position[1]),
+    transform: `scale(${scale})`,
   };
   return (
     <div className={styles.tile} style={style}>
--- constants.ts
+++ constants.ts
@@ -1,2 +1,4 @@
 export const tileCountPerDimesion = 4;
 export const containerWidth = 288; // pixels
+export const mergeAnimationDuration = 100; // ms
+export const moveAnimationDuration = 200; // ms
